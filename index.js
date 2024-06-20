147112bf4a9e1f43f6cbd450d4a28d5696c6412f
Yash Tariyal <yashtariyal4@gmail.com>
2024-06-20 09:07:18 +0530

    intial auth added #1

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const {generateRegistrationOptions} = require('@simplewebauthn/server')
+
+
+const PORT = 3000
+const app = express();
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+const userStore = {}
+const challengeStore = {}
+
+app.post('/register', (req, res) => {
+    const {username, password} = req.body
+    const id = `user_${Date.now()}`
+
+    const user = {
+        id,
+        username,
+        password
+    }
+
+    userStore[id] = user
+
+    console.log(`Register successfull`, userStore[id])
+
+    return res.json({id})
+})
+
+app.post('/register-challenge', async (req, res) => {
+    const {userId} = req.body
+
+    if(!userStore[userId]) return res.status(404).json({ error: 'user not found'})
+    
+    const user = userStore[userId]
+
+    const challengePayload = await generateRegistrationOptions({
+        rpID: 'localhost',
+        rpName: 'My Localhost Machine',
+        userName: user.username
+    })
+
+    challengeStore[userId] = challengePayload.challenge
+
+    return res.json({options: challengePayload })
+})
+app.listen(PORT, () => console.log(`Server Started on PORT:${PORT}`))
